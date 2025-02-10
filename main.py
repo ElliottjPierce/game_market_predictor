0f3aea34f0d87ddcc5379fd052b253d73a4f65b9
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-02-10 14:49:45 -0500

    basic ui

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ class MainApp(tk.Tk):
         super().__init__()
         self.title("Game Market Predictor")
 
-        self.instructions = tk.Label(self, text="Instructions: ", font=('Arial', 18))
-
+        self.instructions = tk.Label(self, text="Instructions: \n Complete the following prompts and click 'Generate' to produce the results.", font=('Arial', 18))
         self.instructions.pack(padx=20, pady=20)
 
+        self.input_path = tk.StringVar(value="path/to/game/data.csv")
+        self.input_path_entry = tk.Entry(self, textvariable=self.input_path)
+        self.input_path_entry.pack()
+
+        self.generator = tk.Button(self, text="Generate", command=self.generate)
+        self.generator.pack()
+
+    def generate(self):
+        print("test" + self.input_path.get())
 
 if __name__ == '__main__':
     app = MainApp()
