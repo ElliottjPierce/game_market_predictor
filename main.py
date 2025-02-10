5cf406c302a7e509655477d3a08c48660476ca2e
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-02-10 14:07:15 -0500

    got basic Ui to run.
    Had to upgrade to Python 3.12 because of a dependency issue with tkinter on mac

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ class MainApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Game Market Predictor")
-        self.mainloop()
+
+        self.instructions = tk.Label(self, text="Instructions: ", font=('Arial', 18))
+
+        self.instructions.pack(padx=20, pady=20)
 
 
 if __name__ == '__main__':
     app = MainApp()
+    app.mainloop()
