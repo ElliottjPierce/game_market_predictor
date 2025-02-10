b849656c3156d625469908566a86c5f4990e771b
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-02-10 15:59:52 -0500

    basic parsing

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,79 @@
 import pandas as pd
 
-def predict_market(path: str):
+class Advantage:
+    def __init__(self, year: int, sales: int = 0, games: int = 0):
+        self.sales = sales
+        self.games = games
+        self.year = year
+
+class GenreRecord:
+    def __init__(self, name: str):
+        self.name = name
+        self.advantages = []
+
+    def get_advantage(self, year: int) -> Advantage:
+        if not self.advantages:
+            advantage = Advantage(year)
+            self.advantages.append(advantage)
+            return advantage
+
+        first_year: int = self.advantages[0].year
+        index: int = year - first_year
+
+        while index < 0:
+            first_year -= 1
+            self.advantages.insert(0, Advantage(first_year))
+            index += 1
+
+        while index >= len(self.advantages):
+            next_year = first_year + len(self.advantages)
+            self.advantages.append(Advantage(next_year))
+
+        return self.advantages[int(index)]
+
+class MarketData:
+    def __init__(self):
+        self.genre_records = {}
+
+    def add_record(self, data, entry_id):
+        try:
+            genre = data["Genre"]
+            record = self.genre_records.get(genre)
+            if record is None:
+                record = GenreRecord(genre)
+                self.genre_records[genre] = record
+
+            advantage = record.get_advantage(data["Year"])
+            advantage.games += 1
+            advantage.sales += data["Global_Sales"]
+        except Exception as err:
+            raise InvalidGameData(entry_id, err)
+
+
+class InvalidGameData(Exception):
+    def __init__(self, entry_id, inner: Exception):
+        super().__init__()
+        self.entry_id = entry_id
+        self.inner = inner
+
+class ParsingError(Exception):
+    def __init__(self, errs: [str]):
+        super().__init__()
+        self.parsing_errors = errs
+
+def predict_market_from_csv(path: str):
+    data = MarketData()
+
     csv = pd.read_csv(path)
+    parsing_errors = []
+    for line in csv.iterrows():
+        if line[0] & 128 > 0:
+            print(f"parsing line {line[0]}")
+        try:
+            data.add_record(line[1], f"row: {line[0]}")
+        except InvalidGameData as err:
+            parsing_errors.append(err.__str__())
+    if parsing_errors:
+        raise ParsingError(parsing_errors)
+
     return "tmp"
--- main.py
+++ main.py
@@ -11,26 +11,27 @@ class MainApp(tk.Tk):
         self.instructions = tk.Label(self, text="Instructions: \n Complete the following prompts and click 'Generate' to produce the results.", font=('Arial', 18))
         self.instructions.pack(padx=20, pady=20)
 
-        self.input_path = tk.StringVar(value="path/to/game/data.csv")
+        # self.input_path = tk.StringVar(value="path/to/game/data.csv")
+        self.input_path = tk.StringVar(value="~/desktop/vgsales.csv")
         self.input_path_entry = tk.Entry(self, textvariable=self.input_path)
         self.input_path_entry.pack()
 
-        self.errors = tk.StringVar()
-        self.errors_display = tk.Label(self, textvariable=self.errors)
-        self.errors_display.pack()
+        self.logs = tk.StringVar()
+        self.log_display = tk.Label(self, textvariable=self.logs)
+        self.log_display.pack()
 
         self.generator = tk.Button(self, text="Generate", command=self.generate)
         self.generator.pack()
 
     def generate(self):
         try:
-            algorithm.predict_market(self.input_path.get())
+            algorithm.predict_market_from_csv(self.input_path.get())
+            self.log("Operation Successful!")
         except Exception as e:
-            self.error(e.__str__())
+            self.log(e.__str__())
 
-
-    def error(self, msg: str):
-        self.errors.set(msg)
+    def log(self, msg: str):
+        self.logs.set(msg)
 
 if __name__ == '__main__':
     app = MainApp()
