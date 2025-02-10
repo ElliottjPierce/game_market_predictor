441887bdcb61d1ebb27ca3a9cfb1c6153af874b4
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-02-10 15:07:21 -0500

    handle errors and set up algorithm

---
+++ algorithm.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def predict_market(path: str):
+    csv = pd.read_csv(path)
+    return "tmp"
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 
+import algorithm
+
+
 class MainApp(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -12,11 +15,22 @@ class MainApp(tk.Tk):
         self.input_path_entry = tk.Entry(self, textvariable=self.input_path)
         self.input_path_entry.pack()
 
+        self.errors = tk.StringVar()
+        self.errors_display = tk.Label(self, textvariable=self.errors)
+        self.errors_display.pack()
+
         self.generator = tk.Button(self, text="Generate", command=self.generate)
         self.generator.pack()
 
     def generate(self):
-        print("test" + self.input_path.get())
+        try:
+            algorithm.predict_market(self.input_path.get())
+        except Exception as e:
+            self.error(e.__str__())
+
+
+    def error(self, msg: str):
+        self.errors.set(msg)
 
 if __name__ == '__main__':
     app = MainApp()
