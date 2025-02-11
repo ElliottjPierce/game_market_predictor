adb9100333b6b3fc84bf252a11b98db3c01ca987
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-02-11 14:39:52 -0500

    building version now works

--- build.py
+++ build.py
@@ -3,6 +3,7 @@ import PyInstaller.__main__
 if __name__ == '__main__':
     PyInstaller.__main__.run([
         'main.py',
+        '--collect-all=matplotlib',
         '--onefile',
         '--windowed',
         '--noconsole',
