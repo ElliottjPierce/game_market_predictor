c29b91da9483026861013e46a4c990be3b67876f
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-02-11 15:06:57 -0500

    optimize the build

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     PyInstaller.__main__.run([
         'main.py',
         '--collect-all=matplotlib',
+        '--optimize=2',
         '--onefile',
         '--windowed',
         '--noconsole',
