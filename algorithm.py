8822e12a1c00a4dfae9760408687ebc56f5b20a4
Elliott Pierce <elliottjpierce2005@gmail.com>
2025-02-11 13:45:58 -0500

    changed theme

--- algorithm.py
+++ algorithm.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.style.use("dark_background")
+
 class Advantage:
     def __init__(self, year: int, sales: int = 0, games: float = 0):
         self.sales = sales
